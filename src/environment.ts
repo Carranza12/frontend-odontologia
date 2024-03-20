56b4873adbd42edbdc61c8d1f3e6f59af337b8d5
ChrisMoreno23 <therealbloodm00n23@gmail.com>
2024-03-20 15:19:03 -0600

    maquetado del filtro de busqueda

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backendHost: 'http://148.212.195.49:3000',
-  frontenHost: 'http://148.212.195.49:4200',
-  /*   backendHost: 'http://localhost:3000',
-    frontenHost: 'http://localhost:4200'  */
+  //backendHost: 'http://148.212.195.49:3000',
+  //frontenHost: 'http://148.212.195.49:4200',
+ backendHost: 'http://localhost:3000',
+    frontenHost: 'http://localhost:4200'
 };
