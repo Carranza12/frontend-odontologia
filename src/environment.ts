87d121b51f2b114c238190a84dac8599cc850505
carranza12 <franckgta3@gmail.com>
2024-06-01 13:12:03 -0600

    Cambios

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //  backendHost: 'http://192.168.1.27:3000',
-  //frontenHost: 'http://192.168.1.27:4200',   
-    backendHost: 'http://localhost:3000',
-  frontenHost: 'http://localhost:4200',   
+    backendHost: 'http://192.168.1.27:3000',
+  frontenHost: 'http://192.168.1.27:4200',   
+  //  backendHost: 'http://localhost:3000',
+  //frontenHost: 'http://localhost:4200',   
 };
